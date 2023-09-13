4b34187d2d8ab2dcdc63ed2ebc144201070f5778
Axel <108530057+zxelt@users.noreply.github.com>
2023-09-13 21:50:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string __key__ = "zxelt_github";
+string __value__;
+Console.WriteLine("\r\n$$$$$$$$\\                     $$\\   $$\\     \r\n\\____$$  |                    $$ |  $$ |    \r\n    $$  / $$\\   $$\\  $$$$$$\\  $$ |$$$$$$\\   \r\n   $$  /  \\$$\\ $$  |$$  __$$\\ $$ |\\_$$  _|  \r\n  $$  /    \\$$$$  / $$$$$$$$ |$$ |  $$ |    \r\n $$  /     $$  $$<  $$   ____|$$ |  $$ |$$\\ \r\n$$$$$$$$\\ $$  /\\$$\\ \\$$$$$$$\\ $$ |  \\$$$$  |\r\n\\________|\\__/  \\__| \\_______|\\__|   \\____/ \r\n                                            \r\n                                            \r\n                                            \r\n");
+
+Console.Write("Please Enter The Password: ");
+__value__ = Console.ReadLine();
+if (__value__ == __key__)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Correct !! ");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Incorect Password !");
+}
